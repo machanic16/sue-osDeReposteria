6caf85fb448131b8b9354c4021b90add8e0b5534
Joel Tovar <jatr160397gmail.com>
2018-12-06 21:48:16 -0300

    ya la pag tiene responsive y un poco de js

--- newjs.js
+++ newjs.js
@@ -1,57 +1,49 @@
 const dulces = document.getElementById('dulces')
 const contacto = document.getElementById('contacto')
 const info = document.getElementById('info')
+const toggle = document.getElementById('toggleMenu')
+const menu = document.getElementById('menu')
 var totalScroll = 0
 const PosisionDeDulces = 550 //las variedades de dulces se ubican a esta cantidad de pixeles
-const PosisionDeContactos = 1200 //lo mismo ocurre aqui
+const PosisionDeContactos = 1900 //lo mismo ocurre aqui
 
+function agregarEventos() {
+    dulces.addEventListener('click', () => {
+        scroll(15, PosisionDeDulces, 10)
+    })
+    contacto.addEventListener('click', () => {
+        scroll(25, PosisionDeContactos, 7)
+    })
+    info.addEventListener('click', () => {
+        scroll(25, PosisionDeContactos, 10)
+    })
 
+    toggle.addEventListener('click', toggleMenu)
 
-function agregarEventos ()
-{
-    dulces.addEventListener('click',scrollDulces)
-    contacto.addEventListener('click',scrollContacto)
-    info.addEventListener('click',scrollInfo)
 }
 
-function scrollDulces()
-{
-    window.scrollBy(0,15)
-    totalScroll+= 15
-    if(totalScroll < PosisionDeDulces)
-    {
-        setTimeout(() => scrollDulces(),10)
-
-    }else{
-        totalScroll = 0
-    }
-}
-
-
-function scrollContacto()
-{
-    window.scrollBy(0,25)
-    totalScroll+= 25
+function scroll(saltos, posicionDeseada, frecuencia) {
+    
+    window.scrollBy(0, 15)
+    totalScroll += saltos
     console.log(totalScroll)
-    if(totalScroll < PosisionDeContactos)
-    {
-        setTimeout(() => scrollContacto(),10)
-
-    }else{
+    if (totalScroll < posicionDeseada) {
+        console.log('entro al condicional')
+        setTimeout(() => scroll(saltos, posicionDeseada, frecuencia), frecuencia)
+        console.log(totalScroll)
+    } else {
         totalScroll = 0
     }
 }
 
-function scrollInfo()
-{
-    window.scrollBy(0,25)
-    totalScroll+= 25
-    if(totalScroll < 550)
+function toggleMenu()
     {
-        setTimeout(() => scrollInfo(),10)
-
-    }else{
-        totalScroll = 0
+        if (menu.classList.contains('toggleMenu')) {
+        console.log(' si tiene')
+        menu.classList.remove('toggleMenu')
+    } else {
+        console.log('no la tiene')
+        menu.classList.add('toggleMenu')
     }
 }
 
